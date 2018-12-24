e7666091247cf46df258e1dcdd0f7a37a9673411
Mubashir Hussain <mubashir0441@gmail.com>
2018-12-24 14:16:53 +0300

    Framework intial changes

---
+++ MyFramework/MyFramework/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  MyFramework
+//
+//  Created by Mubashir Hussain on 12/24/18.
+//  Copyright © 2018 ABC Company. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    
+    private init(){}
+    
+    public static func doSomething() -> String{
+        
+        return "Now we have a response from framework!!!"
+    }
+}
